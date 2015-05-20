381c5c7cf659a3a9823550eba95a0dea508b9e0a
Ethan Wood <ethantimmywood@gmail.com>
2015-05-20 10:25:44 +0100

    m8 it's gr8
    Th first commitment m8, I’m gettin’ married bruv

---
+++ Assets/_Scripts/_HeadBob.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _HeadBob : MonoBehaviour
+{
+	private bool headBob;
+	private Vector3 currentPosition;
+	private Vector3 previousPosition;
+	void Update ()
+	{
+		if (GetComponent<Rigidbody>().velocity.magnitude > 1)
+		{
+			headBob = true;
+		}
+		else
+		{
+			headBob = false;
+		}
+	}
+}
---
+++ Assets/_Scripts/_Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _Player : MonoBehaviour {
+	public float walkSpeed = 50;
+	public float maxSpeed = 100;
+	public float jumpHeight;
+	public float lookSensitivity;
+	public AudioClip footstepSound1;
+	public AudioClip footstepSound2;
+	public AudioClip footstepSound3;
+	public AudioClip footstepSound4;
+	public AudioClip jumpSound;
+	public AudioClip landSound;
+	private Vector3 input;
+	private bool jumping = false;
+	private void jump()
+	{
+		jumping = false;
+	}
+
+	void Start ()
+	{
+
+	}
+	void Update ()
+	{
+		input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		if (jumping)
+		{
+			if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+			{
+				GetComponent<Rigidbody>().AddForce (input * walkSpeed);
+			}
+				if (Input.GetKey(KeyCode.LeftShift))
+			{
+				walkSpeed =+ 100;
+			}
+			else
+			{
+				walkSpeed =+ 50;
+			}
+		}
+		if (Input.GetButtonDown ("Jump"))
+		{
+			GetComponent<Rigidbody>().AddForce(transform.up*jumpHeight);
+			jumping = true;
+			Invoke ("jump", 1.5f);
+			GetComponent<AudioSource>().PlayOneShot(jumpSound);
+		}
+	}
+}
